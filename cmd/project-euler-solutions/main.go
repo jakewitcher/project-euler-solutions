21e83781196db8aaf9e8b0155db062afb25ac41b
Jake Witcher <jjwitcher09@gmail.com>
2020-10-26 15:54:37 -0400

    rename directory for pkg problem1

--- cmd/project-euler-solutions/main.go
+++ cmd/project-euler-solutions/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/jakewitcher/project-euler-solutions/internal/problem-1/problem1"
+	"github.com/jakewitcher/project-euler-solutions/internal/problem1"
 )
 
 func main() {
-
+	FindMultiplesOfThreeAndFiveRange1000()
 }
 
 func FindMultiplesOfThreeAndFiveRange1000() {
 	multiples := problem1.FindMultiplesOfThreeAndFive(1000)
-	fmt.Println("Multiples of 3 and 5, from 1 to 1000")
+	fmt.Println("Multiples of 3 and 5, ranging from 1 to 1000")
 	for _, n := range multiples {
 		fmt.Println(n)
 	}
